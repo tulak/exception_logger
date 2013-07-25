d991e91043f715a2da4c2bbd3ee357fb52ce0ea7
Filip Zachar <tulak@netspace.sk>
2013-07-25 22:38:11 +0200

    Added support for assets pipeline

--- lib/exception_logger/engine.rb
+++ lib/exception_logger/engine.rb
@@ -6,6 +6,10 @@ require "squeel"
 
 module ExceptionLogger
   class Engine < Rails::Engine
+    initializer "precompile", :group => :all do |app|
+      app.config.assets.precompile << "exception_logger.js"
+      app.config.assets.precompile << "exception_logger.css"
+    end
     ## Better: TODO create rake task to copy files to application/public
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
